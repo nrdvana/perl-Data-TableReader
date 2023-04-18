410011d797b492647aa61ec3bf062f4b41a45c06
Michael Conrad <mike@nrdvana.net>
2023-04-17 20:19:19 -0400

    Use more entertaining example in Decoder::IdiotCSV

--- lib/Data/TableReader/Decoder/IdiotCSV.pm
+++ lib/Data/TableReader/Decoder/IdiotCSV.pm
@@ -11,16 +11,18 @@ extends 'Data::TableReader::Decoder::CSV';
 
 =head1 DESCRIPTION
 
-This decoder deals with those special people who think that encoding CSV is
-as simple as
+This decoder is like L<Data::TableReader::Decoder::CSV|::Decoder::CSV>, but can additionally
+parse the garbage resulting from those special people who write "CSV Export" code
+that looks like
 
-  print join(',', map { qq{"$_"} } @row)."\n";
+  print join(",", map qq{"$_"}, @record)."\n";
 
-regardless of their data containing quote characters or newlines, resulting in
-garbage like
+(or rather the equivalent code in Visual Basic or PHP which is what they're
+probably using)  regardless of their data containing quote characters or newlines,
+resulting in garbage like:
 
-  "First Name","Last Name","Nickname"
-  "Joe","Smith",""SuperJoe, to the rescue""
+  "First Name","Last Name","email"
+  "Joseph "Joe","Smith",""Smith, Joe" <jsmith@example.com>"
 
 This can actually be processed by (recent versions of) the L<Text::CSV> module
 with the following configuration:
@@ -34,6 +36,12 @@ with the following configuration:
 
 And so this module is simply a subclass of L<Data::TableReader::Decoder::CSV>
 which provides those defaults to the parser.
+
+How does the parsing work though?  Well, some guesswork and patterns.  It's not super
+reliable, and you should always complain loudly to whoever generated that data,
+unless they're a much larger company than you and would never listen, or went
+out of business a while back, in which case you can justify using this module
+in production.
 
 =cut
 
