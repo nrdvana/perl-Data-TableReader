9acfa2aeaaa58b1e763e03ce0357a13e94bb841e
Michael Conrad <mike@nrdvana.net>
2024-03-22 13:18:35 -0400

    Version 0.013, fix unit test bug

--- lib/Data/TableReader/Decoder/Mock.pm
+++ lib/Data/TableReader/Decoder/Mock.pm
@@ -2,7 +2,7 @@ package Data::TableReader::Decoder::Mock;
 use Moo 2;
 use Carp 'croak';
 use IO::Handle;
-require MRO::Compat if $] < '5.10';
+require MRO::Compat if $] < '5.010';
 
 extends 'Data::TableReader::Decoder';
 
